0982411ac01c94c9223048369165befe42f2ecad
Markus Faerevaag <m@frv.ag>
2016-09-30 08:05:04 +0200

    add error handling and optional force

--- copy_files.sh
+++ copy_files.sh
@@ -1,37 +1,73 @@
 #!/usr/bin/sh
 
-target=${1%/}
+# multiboot usb drive
+# by Markus Færevaag
+# github.com/mfaerevaag/multibootusb
 
+# use absolute path to cp to sirucumcent
+# aliases which can stop from overwriting
+cp=/usr/bin/cp
+
+# files to copy
 files="grub.cfg
-multiboot.6U4YzT"
-
-dirs="grub.d
+multiboot.6U4YzT
+grub.d
 themes"
 
-# check arg
-if [ ! "$target" ]; then
-    echo "no target given (e.g. /mnt/boot/grub/)"
-    exit
+# error func
+function err
+{
+    echo "$1"
+    exit 1
+}
+
+# parse args
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    err "usage: $0 [--force] PATH_TO_GRUB_DIR"
 fi
 
-# check target exist
-if [ ! -d "$target" ]; then
-    echo "target does not exist"
-    exit
+for arg in $@; do
+    if [ "$arg" == "--force" ] || [ "$arg" == "-f" ]; then
+        force=true
+    else
+        target=${arg%/} # remove trailing slash
+    fi
+done
+
+# check args
+if [ ! "$target" ]; then # check target given
+    err "no target given (e.g. /mnt/boot/grub/)"
+elif [ ! -d "$target" ]; then # check target exist
+    err "target does not exist"
+elif [ ! -f "$target/grubenv" ]; then # check sane target
+    err "not a grub folder"
 fi
 
 echo "copying from $PWD to $target..."
 
 # copy files
 for file in $files; do
-    echo -e "\t$PWD/$file -> $target/$file"
-    yes | cp -f $PWD/$file $target/$file
-done
+    echo -ne " * $PWD/$file -> $target/$file"
 
-# copy dirs
-for dir in $dirs; do
-    echo -e "\t$PWD/$dir -> $target/"
-    yes | cp -rf $PWD/$dir $target/
+    if [ -e "$PWD/$file" ]; then
+        if [ "$force" ]; then
+            echo " [FORCE]"
+            /usr/bin/cp -rf $PWD/$file $target/
+        else
+            # read yes or no
+            read -p " [y/N] " yn
+            case $yn in
+                [Yy]* ) ;;
+                "" ) ;;
+                [Nn]* ) continue;;
+                * ) continue;;
+            esac
+
+            /usr/bin/cp -rf $PWD/$file $target/
+        fi
+    else
+        err "$PWD/$file not found"
+    fi
 done
 
 echo "done"
